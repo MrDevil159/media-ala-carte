7b0ae2ff7e7a813ad62e296cfc31c5ccac90e07c
Mehfooz Ahmed <mehfoozmaaz@gmail.com>
2026-04-20 19:39:35 +0530

    master: move to ssr/ssg

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,18 +1,23 @@
-import { effect, inject, Injectable, signal } from '@angular/core';
+import { effect, inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { DOCUMENT } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
   private readonly doc = inject(DOCUMENT);
-  private readonly storage = this.doc.defaultView?.localStorage;
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly isBrowser = isPlatformBrowser(this.platformId);
+  private readonly storage = this.isBrowser ? this.doc.defaultView?.localStorage : null;
 
   readonly isDark = signal(this.storage?.getItem('theme') !== 'light');
 
   constructor() {
     effect(() => {
       const dark = this.isDark();
-      this.doc.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light');
-      this.storage?.setItem('theme', dark ? 'dark' : 'light');
+      if (this.isBrowser) {
+        this.doc.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light');
+        this.storage?.setItem('theme', dark ? 'dark' : 'light');
+      }
     });
   }
 
@@ -20,3 +25,4 @@ export class ThemeService {
     this.isDark.update(v => !v);
   }
 }
+
