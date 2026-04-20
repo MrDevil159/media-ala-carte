9c45a475aff7f2fa515a0e1df19e2dc9dbd9bfdb
Mehfooz Ahmed <mehfoozmaaz@gmail.com>
2026-04-20 17:50:33 +0530

    master: remove unused component

--- src/app/pages/home/journey-section/journey-section.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-journey-section',
-  templateUrl: './journey-section.html',
-  styleUrl: './journey-section.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class JourneySection {}
